242dc03bfd3475f9948244a2be32c8af71f11ee8
Julian Ozel Rose <julian.ozel.rose@outlook.com>
2025-10-20 00:04:36 -0700

    Codebase cleanup

--- CSS-MultiHack-Internal/src/bunnyhop.h
+++ CSS-MultiHack-Internal/src/bunnyhop.h
@@ -5,15 +5,19 @@
 
 namespace bunnyhop
 {
+	const int FLAG_STANDING = 257;
+	const int FLAG_ONGROUND = 263;
+	const int JUMP_PRESS = 6;
+
 	void Bunnyhop(uintptr_t client, uintptr_t localPlayer)
 	{
 		if (GetAsyncKeyState(VK_SPACE))
 		{
 			int flag = *reinterpret_cast<std::uint32_t*>(localPlayer + offset::m_fFlags);
 
-			if (flag == 257 || flag == 263)
+			if (flag == FLAG_STANDING || flag == FLAG_ONGROUND)
 			{
-				*reinterpret_cast<uint32_t*>(client + offset::m_dwForceJump) = 6;
+				*reinterpret_cast<uint32_t*>(client + offset::m_dwForceJump) = JUMP_PRESS;
 			}
 		}
 	}
--- CSS-MultiHack-Internal/src/triggerbot.h
+++ CSS-MultiHack-Internal/src/triggerbot.h
@@ -4,31 +4,37 @@
 
 namespace triggerbot
 {
+	const int MAX_PLAYERS = 64;
+	const int ATTACK_PRESS = 5;
+	const int ATTACK_RELEASE = 4;
+
 	void Triggerbot(uintptr_t client, uintptr_t localPlayer)
 	{
-		int crosshairId = *reinterpret_cast<uint32_t*>(localPlayer + offset::m_iCrosshairId);
+		uint32_t crosshairId = *reinterpret_cast<uint32_t*>(localPlayer + offset::m_iCrosshairId);
 
-		if (crosshairId != 0 && crosshairId <= 64)
+		if (crosshairId == 0 || crosshairId > MAX_PLAYERS)
 		{
-			uintptr_t targetEntity = *reinterpret_cast<std::uintptr_t*>(client + (offset::m_dwEntityList + (crosshairId - 1) * 0x20));
+			return;
+		}
 
-			if (targetEntity)
+		uintptr_t targetEntity = *reinterpret_cast<std::uintptr_t*>(client + (offset::m_dwEntityList + (crosshairId - 1) * 0x20));
+
+		if (targetEntity)
+		{
+			uint32_t targetTeam = *reinterpret_cast<uint32_t*>(targetEntity + offset::m_iTeamNum);
+			uint32_t playerTeam = *reinterpret_cast<uint32_t*>(localPlayer + offset::m_iTeamNum);
+
+			if (playerTeam != targetTeam)
 			{
-				int targetTeam = *reinterpret_cast<uint32_t*>(targetEntity + offset::m_iTeamNum);
-				int playerTeam = *reinterpret_cast<uint32_t*>(localPlayer + offset::m_iTeamNum);
+				int attackFlag = *reinterpret_cast<uint32_t*>(client + offset::m_dwForceAttack);
 
-				if (playerTeam != targetTeam)
+				if (attackFlag == ATTACK_RELEASE)
 				{
-					int attackFlag = *reinterpret_cast<uint32_t*>(client + offset::m_dwForceAttack);
+					*reinterpret_cast<uint32_t*>(client + offset::m_dwForceAttack) = ATTACK_PRESS;
+					std::this_thread::sleep_for(std::chrono::milliseconds(1));
 
-					if (attackFlag == 4)
-					{
-						*reinterpret_cast<uint32_t*>(client + offset::m_dwForceAttack) = 5;
-						std::this_thread::sleep_for(std::chrono::milliseconds(1));
-
-						*reinterpret_cast<uint32_t*>(client + offset::m_dwForceAttack) = 4;
-						std::this_thread::sleep_for(std::chrono::milliseconds(1));
-					}
+					*reinterpret_cast<uint32_t*>(client + offset::m_dwForceAttack) = ATTACK_RELEASE;
+					std::this_thread::sleep_for(std::chrono::milliseconds(1));
 				}
 			}
 		}
