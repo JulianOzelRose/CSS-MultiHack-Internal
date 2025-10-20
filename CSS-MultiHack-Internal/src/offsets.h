e4faed4457be523f9364f55a35fbe9beef68d607
Julian Ozel Rose <julian.ozel.rose@outlook.com>
2025-10-19 17:36:25 -0700

    Update for x64 compatability

--- CSS-MultiHack-Internal/src/offsets.h
+++ CSS-MultiHack-Internal/src/offsets.h
@@ -4,20 +4,20 @@
 
 namespace offset
 {
-	constexpr std::ptrdiff_t m_dwLocalPlayer = 0x4C88E8;
-	constexpr std::ptrdiff_t m_dwEntityList = 0x4D5AE4;
-	constexpr std::ptrdiff_t m_dwForceAttack = 0x4F5D30;
-	constexpr std::ptrdiff_t m_dwForceJump = 0x4F5D24;
-	constexpr std::ptrdiff_t m_angRotation = 0x47C33C;
-	constexpr std::ptrdiff_t m_iNumPlayers = 0x5EC82C;
-	constexpr std::ptrdiff_t m_vecPunchAngle = 0xE48;
-	constexpr std::ptrdiff_t m_iHealth = 0x94;
-	constexpr std::ptrdiff_t m_iTeamNum = 0x9C;
-	constexpr std::ptrdiff_t m_hViewModel = 0x1118;
-	constexpr std::ptrdiff_t m_vecOrigin = 0x290;
-	constexpr std::ptrdiff_t m_fFlags = 0x350;
-	constexpr std::ptrdiff_t m_iCrosshairId = 0x14F0;
-	constexpr std::ptrdiff_t m_hActiveWeapon = 0xD80;
-	constexpr std::ptrdiff_t m_flFlashMaxAlpha = 0x144C;
-	constexpr std::ptrdiff_t m_flFlashMaxDuration = 0x1450;
+	constexpr std::ptrdiff_t m_dwLocalPlayer = 0x5F4B68;
+	constexpr std::ptrdiff_t m_dwEntityList = 0x6098C8;
+	constexpr std::ptrdiff_t m_dwForceAttack = 0x677310;
+	constexpr std::ptrdiff_t m_dwForceJump = 0x677300;
+	constexpr std::ptrdiff_t m_angRotation = 0x53E4E4;
+	constexpr std::ptrdiff_t m_iNumPlayers = 0x6DA960;
+	//constexpr std::ptrdiff_t m_vecPunchAngle = 0xE48;		// Need to find new offset for x64
+	constexpr std::ptrdiff_t m_iHealth = 0xD0;
+	constexpr std::ptrdiff_t m_iTeamNum = 0xD8;
+	//constexpr std::ptrdiff_t m_hViewModel = 0x1118;		// Need to find new offset for x64
+	constexpr std::ptrdiff_t m_vecOrigin = 0x320;
+	constexpr std::ptrdiff_t m_fFlags = 0x440;
+	constexpr std::ptrdiff_t m_iCrosshairId = 0x1B20;
+	//constexpr std::ptrdiff_t m_hActiveWeapon = 0xD80;		// Need to find new offset for x64
+	constexpr std::ptrdiff_t m_flFlashMaxAlpha = 0x144C;	// Need to find new offset for x64
+	constexpr std::ptrdiff_t m_flFlashMaxDuration = 0x1450;	// Need to find new offset for x64
 }
